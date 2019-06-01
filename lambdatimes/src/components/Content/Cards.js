eddc742a6f875dc4bd37528f255f168e42b593a0
Dominic Torruellas <dom@domtorr.com>
2019-06-01 10:31:42 -0700

    mapping over cards prop and creating Card components

--- lambdatimes/src/components/Content/Cards.js
+++ lambdatimes/src/components/Content/Cards.js
@@ -1,14 +1,19 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Card from './Card';
 
-const Cards = props => {
+const generateKey = () => Math.floor(Math.random() * 1000000000) + Date.now();
+
+const Cards = ({ cards }) => {
   return (
     <div className="cards-container">
-      {/* Using the cards prop, map over the list creating a 
+      {/* Using the cards prop, map over the list creating a
           new Card component for each passing the card as the only prop*/}
+      {cards.map(card => (
+        <Card key={generateKey()} card={card} />
+      ))}
     </div>
-  )
-}
+  );
+};
 
 // Make sure you include prop types for all of your incoming props
 
